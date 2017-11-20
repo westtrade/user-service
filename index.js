e2863597bca04b84b3e8ba93d364fa0addf71637
Popov Gennadiy <commerce@westtrade.tk>
2017-11-20 22:16:05 +0300

    UserService class created

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-import ZeroService from 'zeroservice/src/ZeroService';
+import { ZeroService } from 'zeroservice/src/ZeroService';
+
+const properties = Symbol('Service private properties');
+
+
 
 export default class UserService extends ZeroService {
     constructor(initialUserData = {}, ...args) {
         super(...args);
+
+        this[properties] = {
+            rules: [],
+        }
         
         this.initialState = {
             data: initialUserData,
         };
     }
+
+    can(action) {
+        
+    }
+
+    addRule(action, rule) {
+        
+    }
+
+    login(userData = {}) {
+        
+    }
+
+    logout() {
+        
+    }
+
 };
